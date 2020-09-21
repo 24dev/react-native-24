b7c852a931db20360489669c6a85eda90108dfa1
Oleksii <lexkiselov8@gmail.com>
2020-09-21 16:53:57 +0300

    updated image component

--- src/Image.tsx
+++ src/Image.tsx
@@ -1,56 +1,6 @@
-import React from "react";
-import FastImage from "react-native-fast-image";
-import { ImageStyle, ImageURISource } from "react-native";
+import React, { FC } from "react";
+import FastImage, { FastImageProps } from "react-native-fast-image";
 
-export interface ImageProps {
-  width: number;
-  style: ImageStyle | ImageStyle[];
-  source: ImageURISource;
-  onLoad: (args: any) => any;
-}
+const Image: FC<FastImageProps> = (props) => <FastImage {...props} />;
 
-export interface ImageState {
-  height: number;
-}
-
-class Wrap extends React.PureComponent<ImageProps, ImageState> {
-  state = {
-    height: 150
-  };
-
-  handleLoad = (tempWidth: number, tempHeight: number) => {
-    const { width } = this.props;
-    const ratio = tempHeight / tempWidth;
-    const height = width * ratio;
-    this.setState({
-      height
-    });
-  };
-
-  render() {
-    const { width, source, onLoad = () => {}, style = {} } = this.props;
-    const { height } = this.state;
-    return (
-      <FastImage
-        style={[
-          {
-            width,
-            height
-          },
-          style
-        ]}
-        source={{
-          uri: source.uri,
-          priority: FastImage.priority.high
-        }}
-        onLoad={(e: any) => {
-          this.handleLoad(e.nativeEvent.width, e.nativeEvent.height);
-          onLoad(e);
-        }}
-        resizeMode={FastImage.resizeMode.cover}
-      />
-    );
-  }
-}
-
-export default Wrap;
+export default Image;
